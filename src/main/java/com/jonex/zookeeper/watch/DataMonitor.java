108bd087d3003af9d6184635675b1e00a8436570
xubai <xubai@juanpi.com>
2017-10-11 19:07:08 +0800

    zookeeper study

---
+++ src/main/java/com/jonex/zookeeper/watch/DataMonitor.java
@@ -0,0 +1,113 @@
+package com.jonex.zookeeper.watch;
+
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+
+import java.util.Arrays;
+
+/**
+ * @Author jonex [r13ljj@gmail.com]
+ * @Date 2017/10/11 17:17
+ */
+public class DataMonitor implements Watcher, AsyncCallback.StatCallback{
+
+    ZooKeeper zk;
+    String znode;
+    Watcher chainedWatcher;
+    DataMonitorListener listener;
+
+    boolean dead;
+    byte[] prevData;
+
+
+    public DataMonitor(ZooKeeper zk, String znode, Watcher chainedWatcher, DataMonitorListener listener){
+        this.zk = zk;
+        this.znode = znode;
+        this.chainedWatcher = chainedWatcher;
+        this.listener = listener;
+        //检查节点是否存在
+        zk.exists(znode, true, this, null);
+    }
+
+
+    /**
+     * implements StatCallback
+     *
+     * @param reasonCode
+     * @param path
+     * @param ctx
+     * @param stat
+     */
+    public void processResult(int reasonCode, String path, Object ctx, Stat stat) {
+        boolean exists;
+        switch (reasonCode) {
+            case KeeperException.Code.Ok:
+                exists = true;
+                break;
+            case KeeperException.Code.NoNode:
+                exists = false;
+                break;
+            case KeeperException.Code.SessionExpired:
+            case KeeperException.Code.NoAuth:
+                dead = true;
+                listener.closing(reasonCode);
+                return;
+            default:
+                // Retry errors
+                zk.exists(znode, true, this, null);
+                return;
+        }
+
+        byte b[] = null;
+        if (exists) {
+            try {
+                b = zk.getData(znode, false, null);
+            } catch (KeeperException e) {
+                // We don't need to worry about recovering now. The watch
+                // callbacks will kick off any exception handling
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
+        if ((b == null && b != prevData)
+                || (b != null && !Arrays.equals(prevData, b))) {
+            listener.exist(b);
+            prevData = b;
+        }
+    }
+
+    /**
+     * implements Watcher
+     *
+     * @param watchedEvent
+     */
+    public void process(WatchedEvent watchedEvent) {
+        String path = watchedEvent.getPath();
+        if (watchedEvent.getType() == Event.EventType.None) {
+            switch (watchedEvent.getState()) {
+                case SyncConnected:
+                    break;
+                case Expired:
+                    dead = true;
+                    listener.closing(KeeperException.Code.SESSIONEXPIRED.intValue());
+                    break;
+            }
+        } else {
+            if (path != null && path.equals(znode)) {
+                zk.exists(path, true, this, null);
+            }
+        }
+        if (chainedWatcher != null) {
+            chainedWatcher.process(watchedEvent);
+        }
+    }
+
+    public interface  DataMonitorListener {
+
+        void exist(byte[] data);
+
+        void closing(int reasonCode);
+    }
+
+}
---
+++ src/main/java/com/jonex/zookeeper/watch/Executor.java
@@ -0,0 +1,156 @@
+package com.jonex.zookeeper.watch;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * @Author jonex [r13ljj@gmail.com]
+ * @Date 2017/10/11 17:20
+ */
+public class Executor implements Watcher, Runnable, DataMonitor.DataMonitorListener {
+
+    String filename;
+    String[] exec;
+
+    ZooKeeper zk;
+    DataMonitor dataMonitor;
+
+    Process child;
+
+    public Executor(String hostPort, String znode, String filename, String[] exec)throws IOException{
+        zk = new ZooKeeper(hostPort, 3000, this);
+        dataMonitor = new DataMonitor(zk, znode, null, this);
+        this.filename = filename;
+        this.exec = exec;
+    }
+
+    /**
+     * implement DataMonitorListener
+     *
+     * @param data
+     */
+    public void exist(byte[] data) {
+        if (data == null) {
+            if (child != null) {
+                System.out.println("Killing child");
+                child.destroy();
+                try {
+                    child.waitFor();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            child = null;
+        } else {
+            if (child != null) {
+                System.out.println("stopping child");
+                child.destroy();
+                try {
+                    child.waitFor();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                FileOutputStream fos = new FileOutputStream(filename);
+                fos.write(data);
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                System.out.println("starting child");
+                child = Runtime.getRuntime().exec(exec);
+                new StreamWriter(child.getInputStream(), System.out).start();
+                new StreamWriter(child.getErrorStream(), System.out).start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    /**
+     * implement DataMonitorListener
+     *
+     * @param reasonCode
+     */
+    public void closing(int reasonCode) {
+        synchronized (this) {
+            notifyAll();
+        }
+    }
+
+    /**
+     * implement Runnable
+     *
+     */
+    public void run() {
+        try {
+            synchronized (this) {
+                while (!dataMonitor.dead) {
+                    wait();
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * implement Watcher
+     *
+     * @param watchedEvent
+     */
+    public void process(WatchedEvent watchedEvent) {
+        dataMonitor.process(watchedEvent);
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 4) {
+            System.err.println("USAGE: Executor hostPort znode filename program [args ...]");
+            System.exit(2);
+        }
+        String hostPort = args[0];
+        String znode = args[1];
+        String filename = args[2];
+        String exec[] = new String[args.length - 3];
+        System.arraycopy(args, 3, exec, 0, exec.length);
+        try {
+            new Executor(hostPort, znode, filename, exec).run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    static class StreamWriter extends Thread{
+        InputStream is;
+        OutputStream os;
+
+        public StreamWriter(InputStream is, OutputStream os) {
+            this.is = is;
+            this.os = os;
+        }
+
+        @Override
+        public void run() {
+            byte[] buffer = new byte[80];
+            int readBytes = 0;
+            try {
+                while((readBytes = is.read(buffer)) > 0){
+                    os.write(buffer, 0, readBytes);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
